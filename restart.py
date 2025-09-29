8fff47252bfa9db74e13c08e3d7746925a22b040
Max <maxaumuller5@gmail.com>
2025-09-29 08:51:59 +0100

    Rotation now works
    Researched the Vector 2 library:
    https://stackoverflow.com/questions/74349387/how-to-rotate-a-vector2-in-pygame
    
    Researched axis aligned bounding boxes:
    https://gamedev.stackexchange.com/questions/118872/rotating-hitboxes-in-pygame

--- restart.py
+++ restart.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+from pygame import Vector2
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -27,24 +28,42 @@ class Asteroid(pygame.sprite.Sprite):
             self.kill()
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, pos):
         super().__init__()
         self.image = pygame.image.load("ship.png").convert_alpha() #load in the image used for the ship
-        self.image = pygame.transform.scale(self.image, (50, 50))
-        self.rect = self.image.get_rect()
-        self.xPos = x
-        self.yPos = y
-        self.xSpeed = 0
-        self.ySpeed = 0
+        self.image = pygame.transform.scale(self.image, (50, 50)) #make the image the same size as the ship's rectangle
+        self.orig_image = self.image  # Store original image for rotation so quality isn't lost
+        self.rect = self.image.get_rect(center=pos) #place the ship in it's starting position
+        self.pos = Vector2(pos)  #Use a Vector2 for precise positioning
+        self.direction = Vector2(0, -1)  #Facing upward initially
+        self.speed = 0  #Movement speed
+        self.angle = 0  #Rotation angle in degrees
+        self.lives = 3 #the player's lives
+
     def update(self):
-        self.xPos += self.xSpeed
-        self.yPos += self.ySpeed
-        self.xSpeed *= 0.98
-        self.ySpeed *= 0.98
-        self.rect.x = self.xPos
-        self.rect.y = self.yPos
-    def rotate(self,angle):
-        self.rect = pygame.transform.rotate(self.rect, angle)
+        # Move the player in the direction it is facing
+        self.pos += self.direction * self.speed
+        self.rect.center = self.pos  # Keep rect updated so it matches Vector2
+
+        # Apply friction
+        self.speed *= 0.98
+
+        # Screen wrapping
+        if self.rect.top > 900:
+            self.pos.y = 0
+        elif self.rect.bottom < 0:
+            self.pos.y = 900
+        if self.rect.left > 1280:
+            self.pos.x = 0
+        elif self.rect.right < 0:
+            self.pos.x = 1280
+
+    def rotate(self, angle_change):
+        #rotate the player
+        self.angle += angle_change
+        self.image = pygame.transform.rotozoom(self.orig_image, -self.angle, 1) #rotate the image
+        self.rect = self.image.get_rect(center=self.rect.center)  # Keep center position
+        self.direction = Vector2(0, -1).rotate(self.angle)  # Update movement direction
 
 
 pygame.init()
@@ -54,7 +73,7 @@ pygame.display.set_caption("Asteroids")
 
 asteroids = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
-player1 = Player(320, 240) #spawn the play at this position
+player1 = Player((320, 240)) #spawn the play at this position
 all_sprites.add(player1)
 
 done = False
@@ -70,12 +89,12 @@ while not done:
     if keys[pygame.K_ESCAPE]: #quit on escape
         done = True
     if keys[pygame.K_UP]:  # Accelerate forward
-        player1.ySpeed += 0.2  # Gradually increase speed
+        player1.speed += 0.2  # Gradually increase speed
 
     if keys[pygame.K_LEFT]: # rotate left
-        player1.rotate(30)
+        player1.rotate(-5)
     if keys[pygame.K_RIGHT]: # rotate right
-        player1.rotate(-30)
+        player1.rotate(5)
 
     # Spawn asteroids
     if len(asteroids) < 4:
